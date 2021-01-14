84d4ee414eb1617f7b2163d0dd7e2fd9aabb0c5b
yunshao <yunshao0213@gmail.com>
2021-01-14 19:55:36 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WP_2020_01_MID_3A713155
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.tex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn.Location = new System.Drawing.Point(12, 185);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(353, 74);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "分析運勢";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // tex
+            // 
+            this.tex.Font = new System.Drawing.Font("微軟正黑體", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tex.Location = new System.Drawing.Point(12, 77);
+            this.tex.Multiline = true;
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(208, 90);
+            this.tex.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 61);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "輸入星座";
+            // 
+            // pic
+            // 
+            this.pic.Image = global::WP_2020_01_MID_3A713155.Properties.Resources._1;
+            this.pic.Location = new System.Drawing.Point(12, 276);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(435, 203);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(226, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 96);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter\r\n按一次就好";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 491);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.btn);
+            this.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "Form1";
+            this.Text = "星座運勢分析";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox tex;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_MID_3A713155.Properties;
+
+namespace WP_2020_01_MID_3A713155
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 為圖片產生陣列
+        /// </summary>
+        List<Image> res = new List<Image>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            /// <summary>
+            /// 判斷是否空白
+            /// </summary>
+            if (string.IsNullOrWhiteSpace(tex.Text))
+            {
+                MessageBox.Show("請輸入星座", "錯誤警告");
+                return;//重新檢查
+            }
+            else
+            {
+                /// <summary>
+                /// 選擇圖檔
+                /// </summary>
+                switch (tex.Text)
+                {
+                    case "天秤":
+                        pic.Image = res[0];
+                        break;
+                    case "天秤座":
+                        pic.Image = res[0];
+                        break;
+                    case "天蠍":
+                        pic.Image = res[1];
+                        break;
+                    case "射手":
+                        pic.Image = res[2];
+                        break;
+                    case "巨蟹":
+                        pic.Image = res[3];
+                        break;
+                    case "水瓶":
+                        pic.Image = res[4];
+                        break;
+                    case "牡羊":
+                        pic.Image = res[5];
+                        break;
+                    case "獅子":
+                        pic.Image = res[6];
+                        break;
+                    case "處女":
+                        pic.Image = res[7];
+                        break;
+                    case "金牛":
+                        pic.Image = res[8];
+                        break;
+                    case "雙子":
+                        pic.Image = res[9];
+                        break;
+                    case "雙魚":
+                        pic.Image = res[10];
+                        break;
+                    case "魔羯":
+                        pic.Image = res[11];
+                        break;
+                    case "天蠍座":
+                        pic.Image = res[1];
+                        break;
+                    case "射手座":
+                        pic.Image = res[2];
+                        break;
+                    case "巨蟹座":
+                        pic.Image = res[3];
+                        break;
+                    case "水瓶座":
+                        pic.Image = res[4];
+                        break;
+                    case "牡羊座":
+                        pic.Image = res[5];
+                        break;
+                    case "獅子座":
+                        pic.Image = res[6];
+                        break;
+                    case "處女座":
+                        pic.Image = res[7];
+                        break;
+                    case "金牛座":
+                        pic.Image = res[8];
+                        break;
+                    case "雙子座":
+                        pic.Image = res[9];
+                        break;
+                    case "雙魚座":
+                        pic.Image = res[10];
+                        break;
+                    case "魔羯座":
+                        pic.Image = res[11];
+                        break;
+                    default:
+                        MessageBox.Show("文字不符，請重新輸入星座", "錯誤警告");
+                        break;
+                }
+            }
+            return;
+        }
+        /// <summary>
+        /// 載入圖片
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            res.Add(Resources.天秤);
+            res.Add(Resources.天蠍);
+            res.Add(Resources.射手);
+            res.Add(Resources.巨蟹);
+            res.Add(Resources.水瓶);
+            res.Add(Resources.牡羊);
+            res.Add(Resources.獅子);
+            res.Add(Resources.處女);
+            res.Add(Resources.金牛);
+            res.Add(Resources.雙子);
+            res.Add(Resources.雙魚);
+            res.Add(Resources.魔羯);
+        }
+    }
+}
